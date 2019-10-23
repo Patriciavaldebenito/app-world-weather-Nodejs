704ff006c2acb2a07a7be042af43848beb09e353
Patriciavaldebenito <patricia.valdebenito@usach.cl>
2019-10-23 19:36:48 -0300

    Se crea funcion getInfo para obtener mensaje deseado en consola con lugar ingresado y temperatura de este. Se usa try y catch, esta fx  retornara el mensaje o un mensaje de error

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const argv = require('./config/yargs').argv;
-const placeFunction = require('./place-city/place-city'); 
+const placeFunction = require('./place-city/place-city');
 const weatherFunction = require('./weather/weather');
 //testing
 // console.log(argv);
-console.log(argv.direction);
+//console.log(argv.direction);
 
 
 
@@ -12,6 +12,24 @@ console.log(argv.direction);
 // .then(console.log)
 // .catch(console.log)
 
-weatherFunction.getWeather(40.750000,-74.000000)
-.then(console.log)
-.catch(console.log)
+// weatherFunction.getWeather(40.750000,-74.000000)
+// .then(console.log)
+// .catch(console.log)
+
+const getInfo = async (direction) => {
+    //rpa
+    try {
+        let coords = await placeFunction.getPlaceCity(direction)
+        let temp = await weatherFunction.getWeather(coords.lat, coords.lng)
+
+        return `El clima en ${direction} es de ${temp}.`;
+    
+    } catch {
+        throw new Error(`No se pudo determinar el clima de ${direction}`);
+    }
+
+}
+
+getInfo(argv.direction)
+    .then(console.log)
+    .catch(console.log)
--- weather/weather.js
+++ weather/weather.js
@@ -2,9 +2,8 @@ const axios = require('axios');
 
 
 const getWeather = async (lat, lng) => {
-    console.log("corriendo")
+
     let rpta = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lng }&appid=32f4e741ebd6d41a366cf1905dcc76d4&units=metric`);
-  
     return rpta.data.main.temp;
    
 }
